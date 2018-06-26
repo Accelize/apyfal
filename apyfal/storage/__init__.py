6bf1e19744188d9b2436c22c88ebf74f983b142a
jgoutin <jgoutin@accelize.com>
2018-06-26 12:16:59 +0200

    Add apyfal.storage.open, add file-like object support to open and copy.

--- apyfal/storage/__init__.py
+++ apyfal/storage/__init__.py
@@ -1,10 +1,41 @@
 # coding=utf-8
-"""Data storage management"""
+"""Data storage I/O
+
+Support I/O over different kind of storage that must
+be first registered.
+
+Register storage:
+    Storage defined in configuration are automatically registered
+    on first call. It is possible to manually register new storage
+    with "register" function.
+
+Storage URL format:
+    All operations works with URL with format "scheme://path"
+
+    Common schemes are (Registered by default):
+
+    - "file://" or no scheme: Client local file.
+    - "host://": Host local file (Available only on client
+        if client is not host).
+    - "http://" or "https://": File access over HTTP.
+    - "ftp://" or "ftps://": File access over FTP.
+
+    Some storage use advanced same, basic form is
+    "StorageType://path" with StorageType the storage type
+    defining this storage.
+
+    Some storage use sub levels, this is separated from
+    storage type with dot ".":
+    "StorageType.SubLevel://path"
+
+See target storage class documentation for more information.
+"""
 
 from abc import abstractmethod as _abstractmethod
-from shutil import copy as _copy
-import tempfile as _tempfile
-
+from contextlib import contextmanager as _contextmanager
+from io import TextIOWrapper as _TextIOWrapper, open as _io_open
+from shutil import copy as _copy, copyfileobj as _copyfileobj
+from tempfile import SpooledTemporaryFile as _SpooledTemporaryFile
 import apyfal.configuration as _cfg
 import apyfal.exceptions as _exc
 import apyfal._utilities as _utl
@@ -27,6 +58,8 @@ class _StorageHook(dict):
 
     def __missing__(self, storage_type):
         # Try to register if not already exists
+        if storage_type in ('file', 'host', 'stream'):
+            raise ValueError('Invalid storage_type "%s"' % storage_type)
         return self.register(storage_type)
 
     def register(self, storage_type, **parameters):
@@ -65,12 +98,17 @@ def _parse_host_url(url):
     Scheme is returned from host point of view:
     "host" scheme is converted to "file" scheme.
 
+    If URL is a file-like object, returns "stream" as scheme.
+
     Args:
-        url (str): URL to parse
+        url (str or file-like object): URL to parse
 
     Returns:
         tuple of str: (scheme, path)
     """
+    if not isinstance(url, str):
+        return 'stream', url
+
     split_url = url.split('://', 1)
     try:
         # Path with scheme
@@ -88,38 +126,89 @@ def _parse_host_url(url):
     return scheme, url if scheme in ('http', ) else path
 
 
-def copy(source, destination):
+@_contextmanager
+def _io_wrapper(stream, mode, encoding=None, errors=None, newline=None):
+    """"""
+    if "t" in mode:
+        text_stream = _TextIOWrapper(
+            stream, encoding=encoding, errors=errors, newline=newline)
+        yield text_stream
+        text_stream.flush()
+    else:
+        yield stream
+
+
+# Create apyfal.storage.open function, but keep reference to builtin open
+_stdlib_open = open
+
+
+@_contextmanager
+def open(url, mode="rb", encoding=None, errors=None, newline=None):
     """
-    Copy a file from source to destination. Support copy
-    over different kind of storage that must be first
-    registered with "register_storage" function.
-
-    Work with URL with format "scheme://path"
-
-    Common schemes are (Registered by default):
-
-    - "file://" or no scheme: Client local file.
-    - "host://": Host local file (Available only on client
-        if client is not host).
-    - "http://" or "https://": File access over HTTP.
-    - "ftp://" or "ftps://": File access over FTP.
-
-    Some storage use advanced same, basic form is
-    "StorageType://path" with StorageType the storage type
-    defining this storage.
-
-    Some storage use sub levels, this is separated from
-    storage type with dot ".":
-    "StorageType.SubLevel://path"
-
-    See target storage class documentation for more information.
+    Open file and return a corresponding file object.
 
     Args:
-        source (str): Source URL.
-        destination (str): Destination URL.
+        url (str or file-like object): URL or file object to open.
+            Can be storage URL, paths, file-like object.
+        mode (str): Mode in which the file is opened
+            (Works like standard library open mode).
+            Support at least 'r' (read), 'w' (write), 'b' (binary),
+            't' (text) modes, eventually more depending on source file.
+        encoding (str): with text  mode,
+            encoding used to decode or encode the file.
+        errors (str): with text mode, specifies how encoding and
+            decoding errors are to be handled
+        newline (str): Controls how universal newlines mode works.
+
+    Returns:
+        file-like object: Opened object handle
+    """
+    # Parses URL
+    scheme, path = _parse_host_url(url)
+    try:
+        storage = _STORAGE[scheme]
+
+    except ValueError:
+        # Open file as stream
+        if scheme == 'file':
+            with _io_open(path, mode=mode, encoding=encoding,
+                          errors=errors, newline=newline) as stream:
+                yield stream
+
+        # Open Stream
+        elif scheme == 'stream':
+            with _io_wrapper(path, mode, encoding,
+                             errors, newline) as wrapped:
+                yield wrapped
+
+    # Open storage as stream
+    else:
+        with _utl.SpooledTemporaryFile(max_size=1e9) as stream:
+            if 'r' in mode:
+                storage.copy_to_stream(path, stream)
+                stream.seek(0)
+
+            with _io_wrapper(stream, mode, encoding,
+                             errors, newline) as wrapped:
+                yield wrapped
+
+            if 'w' in mode:
+                stream.seek(0)
+                storage.copy_from_stream(stream, path)
+
+
+def copy(source, destination):
+    """
+    Copy a file from source to destination.
+
+    Args:
+        source (str or file-like object): Source URL.
+            Can be storage URL, paths, file-like object.
+        destination (str or file-like object): Destination URL.
+            Can be storage URL, paths, file-like object.
     """
 
-    # Analyses URLs
+    # Parses URLs
     src_scheme, src_path = _parse_host_url(source)
     dst_scheme, dst_path = _parse_host_url(destination)
 
@@ -128,11 +217,33 @@ def copy(source, destination):
         # Local to local
         _copy(src_path, dst_path)
 
-    elif src_scheme == 'file' and dst_scheme != 'file':
+    elif src_scheme == 'stream' and dst_scheme == 'stream':
+        # Stream to stream
+        _copyfileobj(src_path, dst_path)
+
+    elif src_scheme == 'stream' and dst_scheme == 'file':
+        # Stream to local
+        with _stdlib_open(dst_path, 'wb') as dst_file:
+            _copyfileobj(src_path, dst_file)
+
+    elif src_scheme == 'file' and dst_scheme == 'stream':
+        # Local to stream
+        with _stdlib_open(src_path, 'rb') as src_file:
+            _copyfileobj(src_file, dst_path)
+
+    elif src_scheme == 'stream':
+        # Stream to storage
+        _STORAGE[dst_scheme].copy_from_stream(src_path, dst_path)
+
+    elif dst_scheme == 'stream':
+        # Storage to stream
+        _STORAGE[src_scheme].copy_to_stream(src_path, dst_path)
+
+    elif src_scheme == 'file':
         # Local to storage
         _STORAGE[dst_scheme].copy_from_local(src_path, dst_path)
 
-    elif src_scheme != 'file' and dst_scheme == 'file':
+    elif dst_scheme == 'file':
         # Storage to local
         _STORAGE[src_scheme].copy_to_local(src_path, dst_path)
 
@@ -192,7 +303,7 @@ class Storage(_utl.ABC):
             source (str): Source URL.
             local_path (str): Local destination path.
         """
-        with open(local_path, 'wb') as file:
+        with _stdlib_open(local_path, 'wb') as file:
             self.copy_to_stream(source, file)
 
     def copy_from_local(self, local_path, destination):
@@ -203,7 +314,7 @@ class Storage(_utl.ABC):
             local_path (str): Local source path.
             destination (str): Destination URL
         """
-        with open(local_path, 'rb') as file:
+        with _stdlib_open(local_path, 'rb') as file:
             self.copy_from_stream(file, destination)
 
     @_abstractmethod
@@ -258,7 +369,7 @@ class Storage(_utl.ABC):
             destination (str): Destination path
         """
         # TODO: Compute max_size to use instead to use fixed 1GB value (psutil)
-        with _tempfile.SpooledTemporaryFile(max_size=1e9) as stream:
+        with _SpooledTemporaryFile(max_size=1e9) as stream:
             storage.copy_to_stream(source, stream)
             stream.seek(0)
             self.copy_from_stream(stream, destination)
--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,13 +1,7 @@
 # coding=utf-8
 """apyfal.storage tests"""
 
-try:
-    # Python 2
-    from StringIO import StringIO as BytesIO
-except ImportError:
-    # Python 3
-    from io import BytesIO
-
+from io import BytesIO
 from shutil import copyfileobj
 
 
@@ -85,6 +79,79 @@ def test_parse_host_url():
         'storage.name', 'path/on/storage')
 
 
+def test_open(tmpdir):
+    """Tests open"""
+    import apyfal.storage as srg
+
+    srg._STORAGE.clear()
+    content = 'dummy_content'.encode()
+
+    # Mock other storage class
+    class DummyStorage(srg.Storage):
+
+        def __init__(self, *args, **kwargs):
+            """Init storage and create stream"""
+            srg.Storage.__init__(self, *args, **kwargs)
+            self.stream = BytesIO()
+            self.storage_to_storage = False
+
+        def copy_from_stream(self, stream, destination):
+            """Write to storage stream from other stream"""
+            self.stream = BytesIO()
+            copyfileobj(stream, self.stream)
+            self.stream.seek(0)
+
+        def copy_to_stream(self, source, stream):
+            """Write in stream fro storage stream"""
+            self.stream.seek(0)
+            copyfileobj(self.stream, stream)
+
+    srg._STORAGE['dummy'] = storage = DummyStorage('dummy')
+
+    # Tests
+    try:
+        # Local file
+        local_file = tmpdir.join('file.txt')
+        local_file.write(content)
+        with srg.open(str(local_file), 'rb') as stream:
+            assert stream.read() == content
+
+        with srg.open(str(local_file), 'rt') as stream:
+            assert stream.read() == content.decode()
+
+        # Stream:
+        stream_file = BytesIO(content)
+        with srg.open(stream_file, 'rb') as stream:
+            assert stream.read() == content
+
+        stream_file = BytesIO(content)
+        with srg.open(stream_file, 'rt') as stream:
+            assert stream.read() == content.decode()
+
+        # Storage
+        with srg.open('dummy://path', 'wb') as stream:
+            stream.write(content)
+        assert storage.stream.read() == content
+
+        storage.stream = BytesIO()
+        with srg.open('dummy://path', 'wt') as stream:
+            stream.write(content.decode())
+        storage.stream.seek(0)
+        assert storage.stream.read() == content
+
+        storage.stream.seek(0)
+        with srg.open('dummy://path', 'rb') as stream:
+            assert stream.read() == content
+
+        storage.stream.seek(0)
+        with srg.open('dummy://path', 'rt') as stream:
+            assert stream.read() == content.decode()
+
+    # Clear registered storage
+    finally:
+        srg._STORAGE.clear()
+
+
 def test_copy(tmpdir):
     """Tests copy"""
     from apyfal.storage import copy, _STORAGE, Storage
@@ -142,7 +209,6 @@ def test_copy(tmpdir):
     _STORAGE.clear()
     _STORAGE['dummy1'] = DummyStorage('dummy1')
     _STORAGE['dummy2'] = DummyStorage('dummy2')
-    _STORAGE['dummy3'] = DummyStorage('dummy3')
 
     # Tests
     try:
@@ -172,8 +238,42 @@ def test_copy(tmpdir):
         assert tmp_dst.read_binary() == content
         tmp_dst.remove()
 
+        # Stream to stream
+        src_stream = BytesIO(content)
+        dst_stream = BytesIO()
+        copy(src_stream, dst_stream)
+        dst_stream.seek(0)
+        assert dst_stream.read() == content
+
+        # Stream to local
+        assert not tmp_dst.check(file=True)
+        src_stream.seek(0)
+        copy(src_stream, tmp_dst_path)
+        assert tmp_dst.check(file=True)
+        assert tmp_dst.read_binary() == content
+        tmp_dst.remove()
+
+        # Local to stream
+        dst_stream = BytesIO()
+        copy(tmp_src_path, dst_stream)
+        dst_stream.seek(0)
+        assert dst_stream.read() == content
+
+        # Stream to storage
+        src_stream.seek(0)
+        assert not _STORAGE['dummy2'].stream.read()
+        copy(src_stream, 'dummy2://path')
+        assert _STORAGE['dummy2'].stream.read() == content
+
+        # Storage to stream
+        dst_stream = BytesIO()
+        copy('dummy1://path', dst_stream)
+        dst_stream.seek(0)
+        assert dst_stream.read() == content
+
         # Storage to storage (Using temporary file)
         _STORAGE['dummy1'].NAME = None
+        _STORAGE['dummy2'].stream = BytesIO()
         assert not _STORAGE['dummy2'].stream.read()
         copy('dummy1://path', 'dummy2://path')
         assert _STORAGE['dummy2'].stream.read() == content
@@ -182,12 +282,13 @@ def test_copy(tmpdir):
 
         # Storage to storage (Using special method)
         _STORAGE['dummy1'].NAME = 'dummy'
-        _STORAGE['dummy3'].NAME = 'dummy'
-        assert not _STORAGE['dummy3'].stream.read()
-        assert not _STORAGE['dummy3'].storage_to_storage
-        copy('dummy1://path', 'dummy3://path')
-        assert _STORAGE['dummy3'].storage_to_storage
-        assert _STORAGE['dummy3'].stream.read() == content
+        _STORAGE['dummy2'].NAME = 'dummy'
+        _STORAGE['dummy2'].stream = BytesIO()
+        assert not _STORAGE['dummy2'].stream.read()
+        assert not _STORAGE['dummy2'].storage_to_storage
+        copy('dummy1://path', 'dummy2://path')
+        assert _STORAGE['dummy2'].storage_to_storage
+        assert _STORAGE['dummy2'].stream.read() == content
 
     # Clear registered storage
     finally:
