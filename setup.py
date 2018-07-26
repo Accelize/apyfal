15f4673de16dfa35927804f967fdef281854a40d
jgoutin <jgoutin@accelize.com>
2018-07-26 16:58:15 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ PACKAGE_INFO = dict(
         'Accelize Website': 'https://www.accelize.com',
         'Contact': 'https://www.accelize.com/contact',
     },
-    license='Apache',
+    license='Apache License, Version 2.0',
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
     install_requires=['setuptools', 'requests', 'ipgetter', 'psutil'],
     extras_require={
