01e2efd231ce9428fc52d854044e564603629481
jgoutin <jgoutin@accelize.com>
2018-08-13 15:09:12 +0200

    Full iterator tests

--- tests/test_iterators.py
+++ tests/test_iterators.py
@@ -55,3 +55,63 @@ def test_lazy_accelerator():
     # Restore Accelerator
     finally:
         apyfal.Accelerator = apyfal_accelerator
+
+
+def test_iter_accelerators():
+    """Tests iter_accelerators"""
+    from apyfal._iterators import iter_accelerators, _LazyAccelerator
+    import apyfal.host
+    from apyfal.exceptions import HostRuntimeException
+
+    # Initializes some values
+    dummy_config = apyfal.configuration.Configuration()
+    for section in tuple(dummy_config):
+        if 'host' in section:
+            try:
+                del dummy_config._sections[section]
+            except KeyError:
+                continue
+
+    dummy_config['host']['host_type'] = 'include_selected'
+    dummy_config['host.include_subsection']['key'] = '0'
+    dummy_config['host.include_errored']['key'] = '0'
+    dummy_config['host.exclude']['key'] = '0'
+    dummy_prefix = 'prefix'
+    dummy_filters = dict(host_type='include_*', instance_name='0')
+
+    # Mocks Host
+    class Host:
+        """Dummy Host"""
+
+        def __init__(self, config=None, host_type=None, **_):
+            """Checks arguments"""
+            assert config == dummy_config
+            self.host_type = host_type
+            if 'errored' in host_type:
+                raise HostRuntimeException('Error')
+
+        def iter_hosts(self, instance_name_prefix):
+            """Checks arguments and yields fake values"""
+            assert instance_name_prefix == dummy_prefix
+            for host in range(2):
+                yield dict(host_type=self.host_type, instance_name=str(host),
+                           accelerator='accelerator', _repr='_repr')
+
+    apyfal_host_host = apyfal.host.Host
+    apyfal._iterators.Host = Host
+    apyfal.host.Host = Host
+
+    # Tests
+    try:
+        result = list(iter_accelerators(
+            config=dummy_config, instance_name_prefix=dummy_prefix,
+            **dummy_filters))
+        assert len(result) == 2
+        for index, item in enumerate(result):
+            assert isinstance(item, _LazyAccelerator)
+            assert 'include' in item.host._properties['host_type']
+
+    # Restore Host
+    finally:
+        apyfal.host.Host = apyfal_host_host
+        apyfal._iterators.Host = apyfal_host_host
